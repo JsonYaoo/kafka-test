4c3eb35599d44d48618d055d8967b7f1956ca86c
yaocs2 <yaocs2@midea.com>
2021-02-06 16:27:17 +0800

    kafka-test> learn-kafka消费者测试

---
+++ learn-kafka/src/main/java/com/jsonyao/cs/api/constant/Const.java
@@ -0,0 +1,58 @@
+package com.jsonyao.cs.api.constant;
+
+/**
+ * 测试常量: 使用接口定义常量, 属性默认是public static final的
+ */
+public interface Const {
+
+	/**
+	 * quickstart Topic
+	 */
+	public static final String TOPIC_QUICKSTART = "topic-quickstart";
+
+	/**
+	 * normal Topic
+	 */
+	public static final String TOPIC_NORMAL = "topic-normal";
+
+	/**
+	 * interceptor Topic
+	 */
+	public static final String TOPIC_INTERCEPTOR = "topic-interceptor";
+
+	/**
+	 * serial Topic
+	 */
+	public static final String TOPIC_SERIAL = "topic-serial";
+
+	/**
+	 * partition Topic
+	 */
+	public static final String TOPIC_PARTITION = "topic-partition";
+
+	/**
+	 * module Topic
+	 */
+	public static final String TOPIC_MODULE = "topic-module";
+
+	/**
+	 * core Topic
+	 */
+	public static final String TOPIC_CORE = "topic-core";
+
+	/**
+	 * rebalance Topic
+	 */
+	public static final String TOPIC_REBALANCE = "topic-rebalance";
+
+	/**
+	 * mt1 Topic
+	 */
+	public static final String TOPIC_MT1 = "topic-mt1";
+
+	/**
+	 * mt2 Topic
+	 */
+	public static final String TOPIC_MT2 = "topic-mt2";
+	
+}
--- learn-kafka/src/main/java/com/jsonyao/cs/api/quickstart/QuickStartProducer.java
+++ learn-kafka/src/main/java/com/jsonyao/cs/api/quickstart/QuickStartProducer.java
@@ -1,6 +1,7 @@
 package com.jsonyao.cs.api.quickstart;
 
 import com.alibaba.fastjson.JSON;
+import com.jsonyao.cs.api.constant.Const;
 import com.jsonyao.cs.api.entity.User;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
@@ -17,8 +18,8 @@ public class QuickStartProducer {
     public static void main(String[] args) {
         // 1. 配置生产者启动的关键属性参数
         Properties properties = new Properties();
-        /*1.1. 连接kafka集群的服务列表，如果有多个，使用"逗号"进行分隔*/
-        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.1.111:9200");
+        /*1.1. 连接kafka集群的服务列表，如果有多个，使用逗号进行分隔*/
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.1.111:9092");
         /*1.2. 标记kafkaClient的ID*/
         properties.put(ProducerConfig.CLIENT_ID_CONFIG, "quickstart-producer");
         /*1.3. Key序列化器: kafka用于做消息投递计算具体投递到对应的主题的哪一个partition而需要的*/
@@ -28,13 +29,15 @@ public class QuickStartProducer {
 
         // 2. 传递properties属性参数集合, 构造kafka生产者对象
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+        for(int i = 0; i < 10; i++){
+            // 3. 构造消息内容: topic, 实际消息体
+            User user = new User("00" + i, "张三");
+            ProducerRecord<String, String> record = new ProducerRecord<>(Const.TOPIC_QUICKSTART, JSON.toJSONString(user));
 
-        // 3. 构造消息内容: topic, 实际消息体
-        User user = new User("001", "张三");
-        ProducerRecord<String, String> record = new ProducerRecord<>("topic-quickstart", JSON.toJSONString(user));
-
-        // 4. 发送消息
-        producer.send(record);
+            // 4. 发送消息
+            producer.send(record);
+            System.err.println("quickstart producer send....");
+        }
 
         // 5. 关闭生产者
         producer.close();
