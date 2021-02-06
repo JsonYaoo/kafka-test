79df0a01727162cda6491529c872bd52213362a3
yaocs2 <yaocs2@midea.com>
2021-02-06 22:37:49 +0800

    kafka-test> learn-kafka生产者最佳实践

---
+++ learn-kafka/src/main/java/com/jsonyao/cs/api/producer/NormalConsumer.java
@@ -0,0 +1,78 @@
+package com.jsonyao.cs.api.producer;
+
+import com.jsonyao.cs.api.constant.Const;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * Kafka测试生产者
+ */
+public class NormalConsumer {
+
+    public static void main(String[] args) {
+        // 1. 配置属性参数
+        Properties properties = new Properties();
+        /*1.1. 连接kafka集群的服务列表，如果有多个，使用逗号进行分隔*/
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.1.111:9092");
+        /*1.2. 设置订阅组ID, 与消费者订阅组有关系*/
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "normal-group");
+        /*1.3. Key反序列化器*/
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        /*1.4. Value反序列化器*/
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        /*1.5. 设置常规属性: 会话连接超时时间*/
+        properties.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, 10000);
+        /*1.6. 设置常规属性: 自动提交与自动提交周期, 默认不用设置*/
+        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
+        properties.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, 5000);
+
+        // 2. 创建消费者对象
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+        // 3. 订阅感兴趣的主题
+        consumer.subscribe(Collections.singletonList(Const.TOPIC_NORMAL));
+        System.err.println("quickstart consumer started...");
+
+        /*监听消息*/
+        try {
+            while (true){
+                // 4. 采用拉取的方式消费数据
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+                /*遍历所有Partition*/
+                for (TopicPartition topicPartition : records.partitions()) {
+                    /*获取该partition对应的topic*/
+                    String topic = topicPartition.topic();
+                    /*获取该partition下的所有消息*/
+                    List<ConsumerRecord<String, String>> partitionRecords = records.records(topicPartition);
+                    int size = partitionRecords.size();
+
+                    System.err.println(String.format("--- 获取topic: %s, 分区位置：%s, 消息总数： %s", topic, topicPartition.partition(), size));
+
+                    /*遍历当前partition下的所有消息*/
+                    for (ConsumerRecord<String, String> consumerRecord : partitionRecords) {
+                        /*实际消息内容*/
+                        String value = consumerRecord.value();
+                        /*消息偏移量*/
+                        long offset = consumerRecord.offset();
+                        /*当前消息提交的offset: 表示下一次从什么位置开始拉取消息*/
+                        long commitOffset = offset + 1;
+                        System.err.println(String.format("获取实际消息 value：%s, 消息offset: %s, 提交offset: %s", value, offset, commitOffset));
+                    }
+                }
+            }
+        } catch (Exception e){
+            throw e;
+        } finally {
+            consumer.close();
+        }
+    }
+}
---
+++ learn-kafka/src/main/java/com/jsonyao/cs/api/producer/NormalProducer.java
@@ -0,0 +1,84 @@
+package com.jsonyao.cs.api.producer;
+
+import com.alibaba.fastjson.JSON;
+import com.jsonyao.cs.api.constant.Const;
+import com.jsonyao.cs.api.entity.User;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+/**
+ * Kafka测试生产者
+ */
+public class NormalProducer {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        // 1. 配置生产者启动的关键属性参数
+        Properties properties = new Properties();
+        /*1.1. 连接kafka集群的服务列表，如果有多个，使用逗号进行分隔*/
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.1.111:9092");
+        /*1.2. 标记kafkaClient的ID*/
+        properties.put(ProducerConfig.CLIENT_ID_CONFIG, "normal-producer");
+        /*1.3. Key序列化器: kafka用于做消息投递计算具体投递到对应的主题的哪一个partition而需要的*/
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        /*1.4. Value序列化器: 实际发送消息的内容序列化*/
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        /**
+         * 1.5 消息重试机制: 重试次数(默认是0)与重试间隔?
+         *      1) 可重试异常: 意思是执行指定的重试次数, 如果到达重试次数上限还没有发送成功, 也会抛出异常信息
+         *          eg: NetworkException、LeaderNotAvailableException
+         *      2) 不可重试异常: 发生则不会继续重试
+         *          eg: RecordTooLargeException
+         */
+        properties.put(ProducerConfig.RETRIES_CONFIG, 3);
+        // 2. 传递properties属性参数集合, 构造kafka生产者对象
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // 3. 构造消息内容: topic, 实际消息体
+        User user = new User("001", "xiao xiao");
+        ProducerRecord<String, String> record = new ProducerRecord<>(Const.TOPIC_NORMAL, JSON.toJSONString(user));
+
+        /**
+         * ProducerRecord(
+         *      topic=topic-normal,
+         *      partition=null,
+         *      headers=RecordHeaders(headers = [], isReadOnly = false),
+         *      key=null,
+         *      value={"id":"001","name":"xiao xiao"},
+         *      timestamp=null
+         * )
+         * 而一条消息必须通过key 去计算出来实际的partition, 按照partition去存储的, 所以key为空时Send方法会计算出partition
+         */
+        System.err.println("新创建消息：" + record);
+
+        // 4. 发送消息
+        /**
+         * 4.1) 一个参数的send方法: 本质上也是异步的, 返回的是一个future对象, 但调用get()时为同步阻塞方式
+         *     分区：3, 偏移量: 2, 时间戳: 1612621554999
+         */
+//        Future<RecordMetadata> future = producer.send(record);
+//        RecordMetadata recordMetadata = future.get();
+//        System.err.println(String.format("分区：%s, 偏移量: %s, 时间戳: %s", recordMetadata.partition(), recordMetadata.offset(), recordMetadata.timestamp()));
+        /**
+         * 4.2) 带有两个参数的send方法: 是完全异步化的, 在回调Callback方法中得到发送消息的结果
+         *      分区：3, 偏移量: 0, 时间戳: 1612621997593
+         */
+        producer.send(record, new Callback() {
+            @Override
+            public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                if(e != null){
+                    e.printStackTrace();
+                    return;
+                }
+                System.err.println(String.format("分区：%s, 偏移量: %s, 时间戳: %s", recordMetadata.partition(), recordMetadata.offset(), recordMetadata.timestamp()));
+            }
+        });
+
+        // 5. 关闭生产者
+        System.err.println("quickstart producer send....");
+        producer.close();
+    }
+}
