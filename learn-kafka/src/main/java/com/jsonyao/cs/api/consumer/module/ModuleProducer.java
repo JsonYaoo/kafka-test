97acb725f97b046d08dd2908bcc19b7c71d427ba
yaocs2 <yaocs2@midea.com>
2021-02-08 14:29:47 +0800

    kafka-test> learn-kafka测试同组消费者再均衡

---
+++ learn-kafka/src/main/java/com/jsonyao/cs/api/consumer/module/ModuleProducer.java
@@ -0,0 +1,36 @@
+package com.jsonyao.cs.api.consumer.module;
+
+import com.alibaba.fastjson.JSON;
+import com.jsonyao.cs.api.constant.Const;
+import com.jsonyao.cs.api.entity.User;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+/**
+ * Kafka Producer: 测试消费者组
+ */
+public class ModuleProducer {
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.1.111:9092");
+        properties.put(ProducerConfig.CLIENT_ID_CONFIG, "module-producer");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+        for(int i = 0; i < 10; i++){
+            User user = new User("00" + i, "张三");
+            ProducerRecord<String, String> record = new ProducerRecord<>(Const.TOPIC_MODULE, JSON.toJSONString(user));
+
+            producer.send(record);
+            System.err.println("module producer send....");
+        }
+
+        producer.close();
+    }
+}
--- learn-kafka/src/main/java/com/jsonyao/cs/api/consumer/rebalance/RebalanceProducer.java
+++ learn-kafka/src/main/java/com/jsonyao/cs/api/consumer/rebalance/RebalanceProducer.java
@@ -1,4 +1,4 @@
-package com.jsonyao.cs.api.consumer.module;
+package com.jsonyao.cs.api.consumer.rebalance;
 
 import com.alibaba.fastjson.JSON;
 import com.jsonyao.cs.api.constant.Const;
@@ -11,24 +11,24 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import java.util.Properties;
 
 /**
- * Kafka Producer: 测试消费者组
+ * Kafka Producer: 测试同组消费者再均衡
  */
-public class MoudleProducer {
+public class RebalanceProducer {
 
     public static void main(String[] args) {
         Properties properties = new Properties();
         properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.1.111:9092");
-        properties.put(ProducerConfig.CLIENT_ID_CONFIG, "module-producer");
+        properties.put(ProducerConfig.CLIENT_ID_CONFIG, "rebalance-producer");
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
         for(int i = 0; i < 10; i++){
             User user = new User("00" + i, "张三");
-            ProducerRecord<String, String> record = new ProducerRecord<>(Const.TOPIC_MODULE, JSON.toJSONString(user));
+            ProducerRecord<String, String> record = new ProducerRecord<>(Const.TOPIC_REBALANCE, JSON.toJSONString(user));
 
             producer.send(record);
-            System.err.println("module producer send....");
+            System.err.println("rebalance producer send....");
         }
 
         producer.close();
