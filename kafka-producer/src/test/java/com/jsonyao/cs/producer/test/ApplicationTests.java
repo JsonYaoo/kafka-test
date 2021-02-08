c77ccb6b9b193e0b77d310a02769127a3e46d9bd
yaocs2 <yaocs2@midea.com>
2021-02-08 20:29:52 +0800

    kafka-test> kafka-consumer测试实现

---
+++ kafka-producer/src/test/java/com/jsonyao/cs/producer/test/ApplicationTests.java
@@ -0,0 +1,30 @@
+package com.jsonyao.cs.producer.test;
+
+import com.jsonyao.cs.service.KafkaProducerService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Producer测试类
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTests {
+
+    @Autowired
+    private KafkaProducerService kafkaProducerService;
+
+    @Test
+    public void send() throws InterruptedException {
+        String topic = "topic02";
+        for(int i = 0; i < 1000; i++){
+            kafkaProducerService.sendMessage(topic, "hello kafka" + i);
+            Thread.sleep(5);
+        }
+
+        Thread.sleep(Integer.MAX_VALUE);
+    }
+}
