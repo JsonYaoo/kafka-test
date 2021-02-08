a41b17c390225b0bf103839c5e7d6a7825efb72a
yaocs2 <yaocs2@midea.com>
2021-02-08 22:00:33 +0800

    kafka-test> collector设置当前线程局部变量MDC

--- collector/src/main/java/com/jsonyao/cs/util/InputMDC.java
+++ collector/src/main/java/com/jsonyao/cs/util/InputMDC.java
@@ -1,27 +1,33 @@
 package com.jsonyao.cs.util;
 
-import org.jboss.logging.MDC;
+import org.slf4j.MDC;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 /**
- * Log4j2 InputMDC
+ * Log4j2 MDC线程局部变量
  */
 @Component
 public class InputMDC implements EnvironmentAware {
 
-	private static Environment environment;
-	
-	@Override
-	public void setEnvironment(Environment environment) {
-		InputMDC.environment = environment;
-	}
-	
-	public static void putMDC() {
-		MDC.put("hostName", NetUtil.getLocalHostName());
-		MDC.put("ip", NetUtil.getLocalIp());
-		MDC.put("applicationName", environment.getProperty("spring.application.name"));
-	}
+    private static Environment environment;
 
+    /**
+     * 获取并设置environment
+     * @param environment
+     */
+    @Override
+    public void setEnvironment(Environment environment) {
+        InputMDC.environment = environment;
+    }
+
+    /**
+     * 设置MDC线程局部变量
+     */
+    public static void putMDC(){
+        MDC.put("hostName", NetUtil.getLocalHostName());
+        MDC.put("ip", NetUtil.getLocalIp());
+        MDC.put("applicationName", InputMDC.environment.getProperty("spring.application.name"));
+    }
 }
--- collector/src/main/java/com/jsonyao/cs/web/IndexController.java
+++ collector/src/main/java/com/jsonyao/cs/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jsonyao.cs.web;
 
+import com.jsonyao.cs.util.InputMDC;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,23 +10,41 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
 	/**
-	 * [%d{yyyy-MM-dd'T'HH:mm:ss.SSSZZ}]: 日期
-	 * [%level{length=5}]: 级别
-	 * [%thread-%tid]: 线程ID
-	 * [%logger]: logger
-	 * [%X{hostName}]: 自定义配置, 主机名称
-	 * [%X{ip}]: 自定义配置, IP
-	 * [%X{applicationName}]: 自定义配置, 应用名称
-	 * [%F,%L,%C,%M]: 方法, 行号, 列好, 方法名
+	 * 日志格式解释:
+	 * [%d{yyyy-MM-dd'T'HH:mm:ss.SSSZZ}]: 东八区日期
+	 * [%level{length=5}]: 日志级别
+	 * [%thread-%tid]: 当前线程ID
+	 * [%logger]: 当前类即logger
+	 * [%X{hostName}]: 自定义变量, 主机名称
+	 * [%X{ip}]: 自定义变量, IP
+	 * [%X{applicationName}]: 自定义变量, 应用名称
+	 * [%F,%L,%C,%M]: 当前执行的文件, 当前行号, 当前类的全类名, 当前方法名
 	 * [%m] ## '%ex'%n: message、异常、换行
 	 * @return
 	 */
 	@RequestMapping(value = "/index")
 	public String index() {
+		// 设置MDC线程局部变量
+		InputMDC.putMDC();
+
 		log.info("我是一条info日志");
 		log.warn("我是一条warn日志");
 		log.error("我是一条error日志");
 		return "idx";
 	}
 
+	@RequestMapping(value = "/err")
+	public String err(){
+		// 设置MDC线程局部变量
+		InputMDC.putMDC();
+
+		try {
+			int a = 1 / 0;
+		} catch (Exception e){
+			e.printStackTrace();
+			log.error("算术异常", e);
+		}
+
+		return "err";
+	}
 }
